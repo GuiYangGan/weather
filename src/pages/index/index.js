4c3842175ab74b1323c2219f1d976a1f71bbb653
GanGuiYang <150938420@qq.com>
2019-05-14 14:41:26 +0800

    添加图片

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,7 +3,7 @@
 
 import Taro, { Component } from '@tarojs/taro';
 import wx from '@tarojs/taro-weapp';
-import { View, Text, Button } from '@tarojs/components';
+import { View, Text, Image } from '@tarojs/components';
 import { getRequestAjax, errMessage } from '../../utils/withAsync';
 import amapFile from '../../libs/amap-wx';
 import './index.less';
@@ -13,8 +13,10 @@ const amapKey = '4f0637a9697346a488bc63b8714eb9a2';
 export default class Index extends Component {
 
   state = {
-    data: [],
-    canIUse: false
+    weatherInfo: {},
+    basic: {},
+    update: {},
+    userInfo: {}
   }
 
   config = {
@@ -26,7 +28,43 @@ export default class Index extends Component {
   }
 
   componentDidMount () {
-    // this.initPage();
+    const _this = this;
+    wx.checkSession({
+      success(res) {
+        _this.initPage();
+        // session_key 未过期，并且在本生命周期一直有效
+      },
+      fail() {
+        // session_key 已经失效，需要重新执行登录流程
+        wx.login({
+          success(res) {
+            if (res.code) {
+              // 发起网络请求
+              wx.request({
+                url: 'https://api.weixin.qq.com/sns/jscode2session',
+                data: {
+                  appid: 'wxeba507174da77153',
+                  secret: 'bb8e4fa8433c8cab61871bf368d68627',
+                  js_code: res.code,
+                  grant_type: 'authorization_code'
+                },
+                success(res) {
+                  console.log( res )
+                  _this.initPage();
+                },
+                fail(err) {
+                  console.log(err)
+                  errMessage(err);
+                }
+              })
+            } else {
+              console.log('登录失败！' + res.errMsg)
+              errMessage('登录失败！' + res.errMsg);
+            }
+          }
+        });
+      }
+    });
   }
 
   componentWillUnmount () { }
@@ -36,20 +74,25 @@ export default class Index extends Component {
   componentDidHide () { }
 
   initPage = () => {
+    const _this = this;
     wx.getSetting({
       success(res) {
-        if (res.authSetting['scope.userInfo']) {
-          wx.getUserInfo({
-            success(res) {
-              console.log(res.userInfo)
-            }
-          })
-        }
+        // if (res.authSetting['scope.userInfo']) {
+        //   wx.getUserInfo({
+        //     success(res) {
+        //       _this.setState({ userInfo: res.userInfo });
+        //       console.log(res.userInfo)
+        //     }
+        //   })
+        // }
         if (!res.authSetting['scope.record']) {
           wx.getLocation({
             type: 'wgs84',
             success(res) {
-              console.log( res );
+              _this.getWeatherData( res );
+            },
+            fail(err) {
+              errMessage(err);
             }
           });
         }
@@ -57,32 +100,74 @@ export default class Index extends Component {
     });
   };
 
+  getWeatherData = res => {
+    getRequestAjax( 'https://free-api.heweather.net/s6/weather/now', {
+      location: `${res.longitude},${res.latitude}`,
+      // location: '无锡',
+      key: '72613825608142b3ae8890a8b98e04fc'
+    }).then(({ data }) => {
+      console.log(data.HeWeather6[0])
+      this.setState({
+        weatherInfo: data.HeWeather6[0].now,
+        basic: data.HeWeather6[0].basic,
+        update: data.HeWeather6[0].update
+      })
+    }).catch(err => {
+      console.log(err)
+      errMessage( err );
+    }).finally(() => {
+      wx.hideLoading();
+    });
+    // this.map = new amapFile.AMapWX({
+    //   key:amapKey
+    // });
+    // this.map.getWeather({
+    //   success: function(data){
+    //     //成功回调
+    //     console.log(data)
+    //   },
+    //   fail: function(info){
+    //     //失败回调
+    //     console.log(info)
+    //     errMessage(info.errMsg);
+    //   }
+    // });
+  };
+
   render () {
-    const onGotUserInfo = (e) => {
-      console.log(e.detail.errMsg)
-      console.log(e.detail.userInfo)
-      console.log(e.detail.rawData)
-    }
+    const {
+      basic, update, weatherInfo
+    } = this.state;
+    console.log(weatherInfo.cond_code)
     return (
       <View className='index'>
-        <open-data type="userAvatarUrl"></open-data>
-        <open-data type="userNickName"></open-data>
-        <Button
-          type="default"
-          size="mini"
-          className='btn'
-          lang="zh_CN">
-          退出
-        </Button>
-        <Button
-          type="primary"
-          size="mini"
-          open-type="getUserInfo"
-          lang="zh_CN"
-          className='btn'
-          bindgetuserinfo={onGotUserInfo}>
-          获取用户信息
-        </Button>
+        <View className='user'>
+          <open-data
+            style="height:36px;width:36px;display:inline-block;"
+            type="userAvatarUrl" />
+          <open-data  
+            style="font-size:14px;margin-left:16px;"
+            type="userNickName" />
+        </View>
+        <View className='title'>
+          <Text className='city'>{basic.parent_city}</Text>
+          <Text className='time'>{update.loc || ''}更新</Text>
+        </View>
+        <View className='info'>
+          <Text>温度：{weatherInfo.tmp}℃</Text>
+          <Text>
+            实况：
+            {weatherInfo.cond_code ?
+              <image src={`../../../assets/${weatherInfo.cond_code}.png`} />
+              : null
+            }
+            {weatherInfo.cond_txt}
+          </Text>
+          <Text>风向：{weatherInfo.wind_dir}</Text>
+          <Text>风力：{weatherInfo.wind_sc}</Text>
+          <Text>风速：{weatherInfo.wind_spd}公里/小时</Text>
+          <Text>能见度：{weatherInfo.vis}公里</Text>
+        </View>
       </View>
     )
   }
